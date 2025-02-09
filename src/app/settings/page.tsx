c143c7cf39185f510ac7c63315404f9a484c86fa
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2025-02-09 13:53:38 -0800

    Tauri notification test

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,7 +1,23 @@
+'use client';
+
+import { sendNotification } from '@tauri-apps/plugin-notification';
+
+import { Button } from '@/components/ui/button';
+
 export default function Page() {
   return (
     <div className="container mx-auto">
       <h1 className="m-4 text-3xl font-bold">Settings</h1>
+      <Button
+        onClick={() =>
+          sendNotification({
+            title: 'Settings',
+            body: 'This is a test notification!',
+          })
+        }
+      >
+        Notification Test
+      </Button>
     </div>
   );
 }
